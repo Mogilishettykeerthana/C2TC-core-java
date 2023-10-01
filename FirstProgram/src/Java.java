e5a851a409f95deb71007b8c80329a6790523d3f
admin <admin@DESKTOP-MQLV5QV.bbrouter>
2023-10-01 15:03:31 +0530

    first commit

---
+++ FirstProgram/src/Java.java
@@ -0,0 +1,42 @@
+
+public class Java {
+
+	public static void main(String[] args) { 
+		    
+		        
+		        int intValue = 10; // Integer data type
+		        double doubleValue = 3.14; // Double data type
+		        char charValue = 'A'; // Character data type
+		        boolean boolValue = true; // Boolean data type
+		        long longValue = 1000000000L; // Long data type
+		        float floatValue = 2.718f; // Float data type
+		        short shortValue = 32767; // Short data type
+		        byte byteValue = 127; // Byte data type
+
+		        // Output
+		        System.out.println("int: " + intValue);
+		        System.out.println("double: " + doubleValue);
+		        System.out.println("char: " + charValue);
+		        System.out.println("boolean: " + boolValue);
+		        System.out.println("long: " + longValue);
+		        System.out.println("float: " + floatValue);
+		        System.out.println("short: " + shortValue);
+		        System.out.println("byte: " + byteValue);
+
+		        // Reference data types
+		        String stringValue = "Hello, World!"; // String data type
+		        int[] intArray = {1, 2, 3, 4, 5}; // Array data type
+
+		        // Output
+		        System.out.println("String: " + stringValue);
+		        System.out.print("Array: ");
+		        for (int i = 0; i < intArray.length; i++) {
+		            System.out.print(intArray[i] + " ");
+		        }
+		    }
+		
+
+
+	}
+
+ 
